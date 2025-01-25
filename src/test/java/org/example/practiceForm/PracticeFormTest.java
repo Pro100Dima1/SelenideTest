9857f33717006685da2339213e19e23e5a93d12d
Pro100Dima1 <dima.sinkin@yandex.ru>
2025-01-25 18:15:52 +0300

    Add some tests

--- src/test/java/org/example/practiceForm/PracticeFormTest.java
+++ src/test/java/org/example/practiceForm/PracticeFormTest.java
@@ -1,90 +1,96 @@
 package org.example.practiceForm;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byClassName;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.files.DownloadActions.click;
-import static java.awt.SystemColor.control;
+import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeFormTest {
 
-    @BeforeAll
-    public static void setUp() {
+    @BeforeEach
+    public void setUp() {
         ChromeOptions options = new ChromeOptions();
         options.setPageLoadStrategy(PageLoadStrategy.EAGER);
-        Configuration.browser = "firefox";
+        Configuration.browser = "chrome";
         Configuration.browserSize = "1280x800";
-        //Configuration.baseUrl = "https://demoqa.com";
-        //Configuration.holdBrowserOpen = true;
-        //Configuration.pageLoadTimeout = 120000;
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.holdBrowserOpen = true;
         selenideOpen();
     }
 
-    public static void selenideOpen(){
-        //open("/automation-practice-form");
-        open("https://demoqa.com/automation-practice-form", PracticeFormTest.class);
+    @AfterEach
+    public void tearDown() {
+        closeWindow();
+    }
+
+    public static void selenideOpen() {
+        open("/automation-practice-form");
     }
 
     @Tag("Input")
-    @DisplayName("Проверка наличия поля ввода FirstName")
+    @DisplayName("Проверка поля ввода FirstName")
     @Test
-    public void checkFirstNameInput(){
-        $("#firstName").setValue("Qaqw");
+    public void checkFirstNameInput() {
+        $("#firstName").sendKeys("Lesha");
     }
 
     @Tag("Input")
-    @DisplayName("Проверка наличия поля ввода LastName")
+    @DisplayName("Проверка поля ввода LastName")
     @Test
-    public void checkLastNameInput(){
+    public void checkLastNameInput() {
         $("#lastName").isDisplayed();
+        $("#lastName").sendKeys("Miha");
     }
 
     @Tag("Input")
-    @DisplayName("Проверка наличия поля ввода Email")
+    @DisplayName("Проверка поля ввода Email")
     @Test
-    public void checkEmailInput(){
+    public void checkEmailInput() {
         $("#userEmail").isDisplayed();
+        $("#userEmail").sendKeys("dfgds@asdfas");
     }
 
     @Tag("RadioButton")
     @DisplayName("Проверка выбора radioButton Gender Male")
     @Test
-    public void checkGenderRadioButton(){
-        $("#gender-radio-1").setSelected(true);
-    }   // Пока оставлю. Киря поможет с радиобатанном
+    public void checkGenderRadioButton() {
+        $("#genterWrapper").$(byText("Male")).click();
+    }
 
     @Tag("Input")
-    @DisplayName("Проверка наличия поля ввода Mobile Number")
+    @DisplayName("Проверка поля ввода Mobile Number")
     @Test
-    public void checkMobileNumberInput(){
+    public void checkMobileNumberInput() {
         $("#userNumber").isDisplayed();
-        $("#userNumber").setValue("848484");
+        $("#userNumber").sendKeys("848484");
     }
 
     @Tag("Date")
     @DisplayName("Проверка выбора года рождения в Date of Birth")
     @Test
-    public void checkDateOfBirth(){
+    public void checkDateOfBirth() {
         $("#dateOfBirthInput").isDisplayed();
         $("#dateOfBirthInput").click();
-        $("#react-datepicker__day react-datepicker__day--030 react-datepicker__day--outside-month").click();
-        $("#react-datepicker__month-select").selectOption("January");
-        $("#react-datepicker__year-select").selectOption("2025");
+        $("select[class='react-datepicker__month-select']").selectOption("January");
+        $("select[class='react-datepicker__year-select']").selectOption("2025");
+        $("div[class = 'react-datepicker__day react-datepicker__day--030']").click();
     }
 
+    @Tag("CheckBox")
+    @DisplayName("Проверка нажатия на чекбокс Хобби")
+    @Test
+    public void checkHobbiesCheckBox() {
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+    }
 
-
-
-
-
-
+    @Tag("Input")
+    @DisplayName("Проверка ввода в поле Current Address")
+    @Test
+    public void checkCurrentAddress() {
+        $("#currentAddress").sendKeys("Dom");
+    }
 
 }
