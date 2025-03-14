e35650e88742dc14a44fb196358e7dcf4eaf87f7
Pro100Dima1 <dima.sinkin@yandex.ru>
2025-03-14 19:21:49 +0300

    fix test with Faker data

--- src/test/java/org/example/practiceForm/tests/PracticeFromWithFakerDataTest.java
+++ src/test/java/org/example/practiceForm/tests/PracticeFromWithFakerDataTest.java
@@ -1,36 +1,50 @@
 package org.example.practiceForm.tests;
 
+import com.codeborne.selenide.Configuration;
+import com.github.javafaker.Faker;
 import org.example.practiceForm.pages.PracticeFormPage;
-import org.example.practiceForm.utils.Randomsss;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-public class PracticeFromWithFakerDataTest {
-    public Randomsss randomsss = new Randomsss();
+import java.util.Locale;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class PracticeFromWithFakerDataTest extends TestBase {
     public PracticeFormPage practiceFormPage = new PracticeFormPage();
+
+    Faker faker = new Faker(new Locale("ru"));  //библиотека Faker генерит рандомные данные. И можно указать на каком языке это делать
+    String firstName = faker.name().firstName();
+    String lastName = faker.name().lastName();
+    String streetAddress = faker.internet().emailAddress();
+
+    @Test
+    public void testTest() {
+        Configuration.holdBrowserOpen = true;
+    }
 
     @Tag("Input")
     @DisplayName("Проверка поля ввода FirstName")
     @Test
     public void checkFirstNameInput() {
-        String dataName = randomsss.randomizeString(10);
-        practiceFormPage.sendValueFirstNameInput(dataName);
+        practiceFormPage.sendValueFirstNameInput(firstName);
     }
+
 
     @Tag("Input")
     @DisplayName("Проверка поля ввода LastName")
     @Test
     public void checkLastNameInput() {
-        String dataLastName = randomsss.randomizeString(10);
-        practiceFormPage.sendValueLastNameInput(dataLastName);
+        open("/automation-practice-form");
+        practiceFormPage.sendValueLastNameInput(lastName);
     }
 
     @Tag("Input")
     @DisplayName("Проверка поля ввода Email")
     @Test
     public void checkEmailInput() {
-        String dataEmail = randomsss.randomizeEmail(10);
-        practiceFormPage.sendValueEmailInput(dataEmail);
+        open("/automation-practice-form");
+        practiceFormPage.sendValueEmailInput(streetAddress);
     }
 }
